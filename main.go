d3c943310c28faefffd97356d3bade0053e7a863
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2024-10-03 17:42:53 +0300

    Use the version set by goreleaser

--- main.go
+++ main.go
@@ -7,10 +7,14 @@ import (
 	"github.com/utilitywarehouse/tflint-ruleset-kafka-config/rules"
 )
 
+// set by goreleaser at build time: https://goreleaser.com/cookbooks/using-main.version/
+var version = "dev"
+
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
-			Name: "uw-kafka-config",
+			Name:    "uw-kafka-config",
+			Version: version,
 			Rules: []tflint.Rule{
 				rules.NewMskModuleBackendRule(),
 			},
