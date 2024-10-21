e693bff8eb0e0de7914d7f29999d90c53a8ec7f1
Sorin Buliarca <sbuliarca@utilitywarehouse.co.uk>
2024-10-21 15:57:33 +0300

    Finished check for compression type

--- rules/msk_topic_config.go
+++ rules/msk_topic_config.go
@@ -84,10 +84,34 @@ func (r *MskTopicConfigRule) validateTopicConfig(runner tflint.Runner, topic *hc
 		return nil
 	}
 
-	if err := r.validateCompressionType(runner, configAttr); err != nil {
+	/* construct a mapping between the config key and the config KeyPair. This helps in both checking if a key is defined and to propose fixes to the values*/
+	configKeyToPairMap, err := constructConfigKeyToPairMap(configAttr)
+	if err != nil {
+		return err
+	}
+
+	if err := r.validateCompressionType(runner, configAttr, configKeyToPairMap); err != nil {
 		return err
 	}
 	return nil
+}
+
+func constructConfigKeyToPairMap(configAttr *hclext.Attribute) (map[string]hcl.KeyValuePair, error) {
+	configExpr, ok := configAttr.Expr.(*hclsyntax.ObjectConsExpr)
+	if !ok {
+		return nil, fmt.Errorf("could not convert 'config' of type %T to hclsyntax.ObjectConsExpr", configExpr)
+	}
+
+	res := make(map[string]hcl.KeyValuePair, len(configExpr.ExprMap()))
+	for _, pair := range configExpr.ExprMap() {
+		var pk string
+		diags := gohcl.DecodeExpression(pair.Key, nil, &pk)
+		if diags.HasErrors() {
+			return nil, diags
+		}
+		res[pk] = pair
+	}
+	return res, nil
 }
 
 const (
@@ -157,23 +181,21 @@ func (r *MskTopicConfigRule) reportMissingReplicationFactor(runner tflint.Runner
 
 const (
 	compressionTypeKey = "compression.type"
-	compressionTypeVal = `zstd`
+	compressionTypeVal = "zstd"
 )
 
 var compressionTypeFix = fmt.Sprintf(`"%s" = "%s"`, compressionTypeKey, compressionTypeVal)
 
-func (r *MskTopicConfigRule) validateCompressionType(runner tflint.Runner, config *hclext.Attribute) error {
-	var configValue map[string]string
-	diags := gohcl.DecodeExpression(config.Expr, nil, &configValue)
-	if diags.HasErrors() {
-		return diags
-	}
-
-	compressionType, hasCompressionType := configValue[compressionTypeKey]
-	if !hasCompressionType {
+func (r *MskTopicConfigRule) validateCompressionType(
+	runner tflint.Runner,
+	config *hclext.Attribute,
+	configPairMap map[string]hcl.KeyValuePair,
+) error {
+	ctPair, hasCt := configPairMap[compressionTypeKey]
+	if !hasCt {
 		err := runner.EmitIssueWithFix(
 			r,
-			fmt.Sprintf("missing compression.type: it must be equal to '%s'", compressionTypeVal),
+			fmt.Sprintf("missing %s: it must be equal to '%s'", compressionTypeKey, compressionTypeVal),
 			config.Range,
 			func(f tflint.Fixer) error {
 				return f.InsertTextAfter(config.Expr.StartRange(), "\n"+compressionTypeFix)
@@ -185,43 +207,25 @@ func (r *MskTopicConfigRule) validateCompressionType(runner tflint.Runner, confi
 		return nil
 	}
 
-	configExpr, ok := config.Expr.(*hclsyntax.ObjectConsExpr)
-	if !ok {
-		return fmt.Errorf("could not convert 'config' of type %T to hclsyntax.ObjectConsExpr", configExpr)
+	var ctVal string
+	diags := gohcl.DecodeExpression(ctPair.Value, nil, &ctVal)
+	if diags.HasErrors() {
+		return diags
 	}
 
-	if compressionType != compressionTypeVal {
-		ctPair, err := r.findPair(configExpr, compressionTypeKey)
-		if err != nil {
-			return err
-		}
-
-		err = runner.EmitIssueWithFix(
+	if ctVal != compressionTypeVal {
+		err := runner.EmitIssueWithFix(
 			r,
-			fmt.Sprintf("the compression.type value must be equal to '%s'", compressionTypeVal),
+			fmt.Sprintf("the %s value must be equal to '%s'", compressionTypeKey, compressionTypeVal),
 			ctPair.Key.Range(),
 			func(f tflint.Fixer) error {
 				return f.ReplaceText(ctPair.Value.Range(), `"`+compressionTypeVal+`"`)
 			},
 		)
 		if err != nil {
-			return fmt.Errorf("emitting issue with fix: no replication factor: %w", err)
+			return fmt.Errorf("emitting issue with fix: wrong compression type: %w", err)
 		}
 		return nil
 	}
 	return nil
-}
-
-func (r *MskTopicConfigRule) findPair(configExpr *hclsyntax.ObjectConsExpr, key string) (*hcl.KeyValuePair, error) {
-	for _, pair := range configExpr.ExprMap() {
-		var pk string
-		diags := gohcl.DecodeExpression(pair.Key, nil, &pk)
-		if diags.HasErrors() {
-			return nil, diags
-		}
-		if pk == key {
-			return &pair, nil
-		}
-	}
-	return nil, nil
 }
